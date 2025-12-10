40dc7d94f6324150eaea4e1f5a267f6ceee5c416
Estim <2423860442@qq.com>
2025-12-10 20:52:40 +0800

    动态库

--- src/102cmake_lib/xlog/xlog.h
+++ src/102cmake_lib/xlog/xlog.h
@@ -1,7 +1,20 @@
 //xlog.h
 #ifndef XLOG_H
 #define XLOG_H
-class XLog
+//__declspec(dllexport) 导出xlog类的函数到lib文件中
+//xlog库文件调用使用dllexport
+//test_xlog可执行文件调用使用dllimport
+#ifndef _WIN32
+	#define XCPP_API
+#else
+	#ifdef xlog_EXPORTS
+		#define XCPP_API __declspec(dllexport)
+	#else
+		#define XCPP_API __declspec(dllimport)
+	#endif // xlog_EXPORTS
+#endif
+
+class XCPP_API XLog
 {
 public:
 	XLog();
