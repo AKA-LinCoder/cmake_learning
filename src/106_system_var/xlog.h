c40dac53b1ac50c4746ccda82989349471854a79
Estim <2423860442@qq.com>
2025-12-11 10:29:36 +0800

    系统变量使用

---
+++ src/106_system_var/xlog.h
@@ -0,0 +1,27 @@
+//xlog.h
+#ifndef XLOG_H
+#define XLOG_H
+//__declspec(dllexport) 导出xlog类的函数到lib文件中
+//xlog库文件调用使用dllexport
+//test_xlog可执行文件调用使用dllimport
+#ifndef _WIN32
+	#define XCPP_API
+#else
+	#ifdef  xlog_STATIC //静态库
+		#define XCPP_API
+	#else //动态库
+		#ifdef xlog_EXPORTS
+			#define XCPP_API __declspec(dllexport)
+		#else
+			#define XCPP_API __declspec(dllimport)
+		#endif // xlog_EXPORTS
+	#endif //  xlog_STATIC
+#endif
+
+
+class XCPP_API XLog
+{
+public:
+	XLog();
+};
+#endif
