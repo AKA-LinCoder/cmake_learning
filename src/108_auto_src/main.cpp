f83480e3a377815c28cccad03abcb04f587ce57f
Estim <2423860442@qq.com>
2025-12-12 10:00:21 +0800

    自动导入源码文件

---
+++ src/108_auto_src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "xlog.h"
+using namespace std;
+
+int main() {
+	xlog();
+	cout << "-------------Hello, This is main.cpp-------------" << endl;
+	return 0;
+}
---
+++ src/108_auto_src/src/xlog.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "xlog.h"
+using namespace std;
+void xlog() {
+	cout << "Hello, This is from xlog" << endl;
+}
