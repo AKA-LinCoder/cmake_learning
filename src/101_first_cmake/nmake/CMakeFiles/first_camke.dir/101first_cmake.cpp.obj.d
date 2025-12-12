7e303ee71a9e8a9bbb0aac6dd259163a54b420e5
Estim <2423860442@qq.com>
2025-12-12 11:11:49 +0800

    win环境下知道cmake编译步骤

---
+++ src/101_first_cmake/nmake/CMakeFiles/first_camke.dir/101first_cmake.cpp.obj.d
@@ -0,0 +1,44 @@
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/yvals_core.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/vcruntime.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/__msvc_ostream.hpp
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/xlocnum
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/limits.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/cmath
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/crtdbg.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/vcruntime_new_debug.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/vcruntime_new.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/crtdefs.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/use_ansi.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/cstdlib
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/math.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/corecrt_math.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/corecrt_malloc.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/stddef.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/intrin0.inl.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/stdio.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/cstring
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/wchar.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/corecrt_share.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/sys/types.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/wmmintrin.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/smmintrin.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/tmmintrin.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/emmintrin.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/__msvc_iter_core.hpp
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/initializer_list
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/cstddef
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/stdint.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/xiosbase
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/system_error
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/stdexcept
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/vcruntime_exception.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/__msvc_string_view.hpp
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/xthreads.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/__msvc_threads_core.hpp
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/xtimec.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/ctime
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/memory
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/vcruntime_typeinfo.h
+D:/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/include/__msvc_xlocinfo_types.hpp
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/ctype.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.26100.0/ucrt/locale.h
---
+++ src/101_first_cmake/nmake/CMakeFiles/first_camke.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_camke.dir/101first_cmake.cpp.obj"
+  "CMakeFiles/first_camke.dir/101first_cmake.cpp.obj.d"
+  "first_camke.exe"
+  "first_camke.exe.manifest"
+  "first_camke.lib"
+  "first_camke.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_camke.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
